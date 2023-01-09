8dff90cd38393ef0344a4009b9e032dbdfea2557
Muhammad Saefudin <itsapud@gmail.com>
2023-01-09 09:10:02 +0700

    Day 6 - Add New project

---
+++ DotnetEfCore/dotnet-ef-core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet_ef_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
